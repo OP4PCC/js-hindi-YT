8473cdb7af35d90d6bc7e4a109d6933c390a5e6f
Om Prakash Kumar <50577361+OP4PCC@users.noreply.github.com>
2025-05-20 11:41:15 +0000

    conversion understanding

--- 01-basics/03_conversionOperations.js
+++ 01-basics/03_conversionOperations.js
@@ -25,3 +25,50 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " om"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(typeof ("1" + 2));
+// console.log(typeof (1 + "2"));
+// console.log(typeof ("1" + 2 + 2));
+// console.log(typeof (1 + 2 + "2"));
+
+// console.log( 3 + 4 * 5 % 3);
+// The order of operations (precedence) is:
+
+// *, /, % (Multiplication, Division, Modulus)
+// +, - (Addition, Subtraction)
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
