190964b0872205c98e0002dee4ad5f6967d8990b
Om Prakash Kumar <50577361+OP4PCC@users.noreply.github.com>
2025-05-20 10:21:33 +0000

    understand dataTypes

---
+++ 01-basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version. that tells the JavaScript engine to run the code in strict mode.
+
+
+// alert("Hello") //The alert() function is only available in browsers, not in Node.js.
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+let a = 10
+let b = "om"
+console.log(typeof a)
+console.log(typeof b)
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
