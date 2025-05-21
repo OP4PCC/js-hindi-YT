71865f95355f2449610f1bce1205bbf5a00aa4c6
Om Prakash Kumar <50577361+OP4PCC@users.noreply.github.com>
2025-05-21 02:10:25 +0000

    array part 1

---
+++ 02_basics/01_array.js
@@ -0,0 +1,46 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myar = new Array(7,8,9);
+let arr = [1,2,3,4]
+let nAr = new Array(11,12,13,14)
+
+// console.log(myArr,myar,arr, nAr)
+console.table([myArr,myar,arr, nAr])
+
+// const myHeors = ["shaktiman", "naagraj"]
+
+// const myArr2 = new Array(1, 2, 3, 4)
+// // console.log(myArr[1]);
+
+// // Array methods
+
+// // myArr.push(6)
+// // myArr.push(7)
+// // myArr.pop()
+
+// // myArr.unshift(9)
+// // myArr.shift()
+
+// // console.log(myArr.includes(9));
+// // console.log(myArr.indexOf(3));
+
+// // const newArr = myArr.join()
+
+// // console.log(myArr);
+// // console.log( newArr);
+
+
+// // slice, splice
+
+// console.log("A ", myArr);
+
+// const myn1 = myArr.slice(1, 3)
+
+// console.log(myn1);
+// console.log("B ", myArr);
+
+
+// const myn2 = myArr.splice(1, 3)
+// console.log("C ", myArr);
+// console.log(myn2);
